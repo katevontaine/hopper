3bc423ca6e710a69df74ed522905ddd0099b976d
Jacob Sharp <Jacob@Jacobs-Work-Comp.local>
2015-11-03 16:22:25 -0500

    working on messages

--- app.js
+++ app.js
@@ -2,15 +2,27 @@ var page = {
   usersUrl: "http://tiny-tiny.herokuapp.com/collections/hopper",
   messagesUrl: "http://tiny-tiny.herokuapp.com/collections/hopper-messages",
   init: function(){
-
+    page.stylesInit();
+    page.eventsInit();
   },
   eventsInit: function(){
-
+    $('.messageForm').on('submit', function(event){
+      var messageData = {message: $('input[name="inputMessage"]').val(), author: '', color: ''};
+      event.preventDefault();
+      $.ajax({
+        method:'POST',
+        url: page.messagesUrl,
+        data: messageData,
+        success: function(data){
+          console.log(data);
+        }
+      });
+    });
   },
   stylesInit: function(){
 
   }
 };
 $(document).ready(function(){
-  
+  page.init();
 });
