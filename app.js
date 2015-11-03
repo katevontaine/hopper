186743027f80116be4ac41cc78e4ecca44174a85
Jet Balagtas <jetbalagtas@gmail.com>
2015-11-03 17:01:05 -0500

    added create new user

--- app.js
+++ app.js
@@ -2,31 +2,28 @@ var page = {
   usersUrl: "http://tiny-tiny.herokuapp.com/collections/hopper",
   messagesUrl: "http://tiny-tiny.herokuapp.com/collections/hopper-messages",
   init: function(){
-
+    page.stylesInit();
+    page.eventsInit();
   },
   eventsInit: function(){
-
+    $('.userForm').on('submit', function(event){
+          var userData = {message: $('input[name="inputUser"]').val(), color: ''};
+          event.preventDefault();
+          $.ajax({
+            method:'POST',
+            url: page.usersUrl,
+            data: userData,
+            success: function(data){
+              console.log(data);
+            }
+          });
+        });
   },
   stylesInit: function(){
 
   },
-  createNewUser: function(user) {
-    console.log("New User: ", user);
-    $.ajax({
-      url: page.usersUrl,
-      method: 'POST',
-      data: hopper,
-      success: function(resp) {
-        $('.profile').append(user);
-        $('form > input[type="text"]').val('');
-      },
-      failure: function(resp) {
-        console.log("FAILURE", resp);
-      }
-    });
-  },
-
 };
-$(document).ready(function(){
 
+$(document).ready(function(){
+  page.init();
 });
