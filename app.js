472cc6f0c453322bbaba7b6868052125dded53d0
Blake Guilloud <fbguillo@gmail.com>
2015-11-03 16:05:44 -0500

    added some css or something

--- app.js
+++ app.js
@@ -2,15 +2,25 @@ var page = {
   usersUrl: "http://tiny-tiny.herokuapp.com/collections/hopper",
   messagesUrl: "http://tiny-tiny.herokuapp.com/collections/hopper-messages",
   init: function(){
-
+    page.eventsInIt();
+    page.stylesInIt();
   },
   eventsInit: function(){
 
   },
   stylesInit: function(){
 
+  },
+  deleteUser: function(userId){
+    $.ajax({
+      url: page.usersUrl + '/' + userId,
+      method: 'DELETE',
+      success: function(response){
+        console.log(response)
+      }
+    })
   }
 };
 $(document).ready(function(){
-  
+    page.init();
 });
