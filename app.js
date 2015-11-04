1d865d0f066e3e1ee00fdfd982f183cd4f9da924
Blake Guilloud <fbguillo@gmail.com>
2015-11-04 09:06:18 -0500

    commiting before merge

--- app.js
+++ app.js
@@ -37,23 +37,45 @@ var page = {
         });
   },
   deleteUser: function(){
+    $('body').on('click','.delete',function(event){
+      event.preventDefault();
+    $(this).closest('li').remove();
+      console.log(this)
     $.ajax({
+      userId: '',
       method: 'GET',
       url: page.usersUrl,
       success: function(data){
-        _.each(data,function(el){
 
-        })
+
       }
+    }),
+  $('body').on('click','.delete', function(event){
+    _.each(data,function(el){
+      console.log(el)
+      dataID=el._id
+    }),
+    event.preventDefault();
+        $.ajax({
+          method:'DELETE',
+          URL: page.usersUrl + '/' + dataID,
+          success: function(data){
+            console.log('blue')
+          },
+          failure: function(data){
+            console.log('red')
+          }
+        })
     })
-  },
+  })
+},
   getUserNames: function(){
     $.ajax({
      method: "GET",
      url: page.usersUrl,
      success: function(data){
        _.each(data, function(el){
-         $('.users').append(el.message+'<br>');
+         $('.users').append('<li>'+el.message+'<button class="delete">Delete</button>'+'<br>'+'</li>');
        })
       },
     });
@@ -85,7 +107,8 @@ var page = {
         messagesArr.reverse();
         _.each(messagesArr, function(el){
           console.log(el.message);
-            $('.messages').html(el.message + '<br>')
+            $('.messages').append(el.message +'<br>');
+            $('.messages').val('')
         });
       }
     });
