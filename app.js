ec5b0f0d3b2f7965e71feafd87a64fc4f1c361b3
Blake Guilloud <fbguillo@gmail.com>
2015-11-04 17:16:20 -0500

    blue

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var page = {
   messArr: [],
   currUser: '',
   usersUrl: "http://tiny-tiny.herokuapp.com/collections/hopper",
-  messagesUrl: "http://tiny-tiny.herokuapp.com/collections/lovelace-messages",
+  messagesUrl: "http://tiny-tiny.herokuapp.com/collections/hopper-messages",
 
   init: function(){
     page.editUser();
