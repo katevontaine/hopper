8354577aa02cc965a6e5343d6ab4257b4d67257a
Jacob Sharp <sidewayseightgmod@gmail.com>
2015-11-04 16:15:15 -0500

    fixed delete button

--- app.js
+++ app.js
@@ -2,27 +2,35 @@
 var userId;
 var dataID;
 var loadMessages;
+var loadUsers;
 var page = {
   userArr: [],
+  messArr: [],
   currUser: '',
   usersUrl: "http://tiny-tiny.herokuapp.com/collections/hopper",
   messagesUrl: "http://tiny-tiny.herokuapp.com/collections/hopper-messages",
 
   init: function(){
-    page.stylesInIt();
-    page.eventsInIt();
+    page.eventsInit();
     setInterval(function(){
+      page.stylesInit();
       page.getMessage();
+      page.getUsernames();
     }, 2000);
+    setInterval(function(){
+      page.deleteMessage();
+    }, 10);
   },
-  eventsInIt: function(){
-    page.postMessage();
+  eventsInit: function(){
     page.postUser();
-    page.postAuthor();
-    page.deleteMessage();
+    page.postMessage();
   },
-  stylesInIt: function(){
-    page.getUsernames();
+  stylesInit: function(){
+    _.each(page.userArr, function(el){
+        if(page.currUser === el._id){
+          userId = el.user;
+        }
+      });
   },
   postUser: function(){
     $('.userForm').on('submit', function(event){
@@ -33,25 +41,20 @@ var page = {
             url: page.usersUrl,
             data: userData,
             success: function(data){
+              console.log(data._id);
               page.currUser = data._id;
               $('input[name="inputUser"]').val('');
             }
           });
         });
   },
-  postAuthor: function(){
-    $('.userForm').on('submit', function(event){
-          userId = $('input[name="inputUser"]').val();
-    });
-  },
-  deleteUser: function(){
-    $('body').on('click','.delete',function(event){
+  deleteMessage: function(){
+    _.each(page.messArr, function(el, idx, arr){
+      cls = '.delete-' + idx;
+    $(cls).on('click', function(event){
       event.preventDefault();
-    $(this).closest('li').remove();
-      var dataID = $(this).closest('li').data();
-      console.log('DATAID', dataID);
           $.ajax({
-            url: page.messagesUrl + '/' + dataID.dataid,
+            url: page.messagesUrl + '/' + arr[idx]._id,
             method:'DELETE',
             success: function(blue){
               console.log("SUCCESS BLUE", blue);
@@ -59,6 +62,7 @@ var page = {
             failure: function(data){
             }
           });
+        });
 });
   $('body').on('click','.delete', function(event){
     event.preventDefault();
@@ -69,8 +73,9 @@ var page = {
     $('.messageForm').on('submit', function(event){
       var messageData = {
         message: $('input[name="inputMessage"]').val(),
-        author: $('input[name="author"]').val(),
-        color: ''
+        author: userId,
+        author_id: page.currUser,
+        color: '',
       };
       $('input[name="inputMessage"]').val('');
       event.preventDefault();
@@ -82,15 +87,6 @@ var page = {
         }
       });
     });
-    // $('body').on('click', function(event){
-    //   event.preventDefault();
-    //   $.ajax({
-    //     url: page.usersUrl + '/' + userArr[0]._id,
-    //     method: 'DELETE',
-    //     success: function(data){
-    //     }
-    //   });
-    // });
   },
   getMessage: function(){
     $.ajax({
@@ -99,8 +95,13 @@ var page = {
       success: function(messagesArr){
         messagesArr.reverse();
         loadMessages = '';
-        _.each(messagesArr, function(el){
-          loadMessages += "<li data-dataID="+ el._id +">" + el.message + ": " + userId + '<button class="delete">Delete</button>' + '</br>' + "</li>";
+        page.messArr = messagesArr;
+        _.each(messagesArr, function(el, idx){
+          loadMessages += "<li data-dataID="+ el._id +">" + el.message + ": " + el.author + '</br>' + "</li>";
+          console.log(el.author);
+          if(userId === el.author){
+            loadMessages += '<button class="delete-'+ idx + ' hidden">Delete</button>';
+          }
         });
         $('.messages').html('');
         $('.messages').append(loadMessages);
@@ -112,26 +113,16 @@ getUsernames: function(){
       method: "GET",
       url: page.usersUrl,
       success: function(data){
+        loadUsers = '';
+        page.userArr = data;
         _.each(data, function(el){
-          page.userArr = data;
-          $(".users").append(el.user+ "<br>");
+          loadUsers += el.user + "<br/>";
         });
+        $('.users').html('');
+        $('.users').append(loadUsers);
       },
     });
   },
-deleteMessage: function(){
-   $('body').on('click','.delete',function(event){
-     event.preventDefault();
-   $(this).closest('li').remove();
-   $.ajax({
-     userId: '',
-     method: 'GET',
-     url: page.usersUrl,
-     success: function(data){
-     }
-   });
- });
-},
 };
 $(document).ready(function(){
  page.init();
